0cef4da8e34766cc412c0aa3bb56b715836a4b0d
nubskr <dakshrinwan2003@gmail.com>
2024-10-01 03:51:45 +0530

    more, something is broken with resizing tho

--- hello.go
+++ hello.go
@@ -8,8 +8,34 @@ import (
     "sync/atomic"
     "sync"
     "strings"
-    // "runtime"
+    "runtime"
 )
+
+/*
+TODO: 
+
+- dynamic shard initializations (double when full) ; O(1) asymptomatic time in the end
+(put a mutex lock only while it is being copied, make sure no changes occur!!!)
+
+writes to new shards are blocked while resizing is happening
+
+how does this changes reads ?
+
+make copy in background and just change the pointer to shardmanager when its done, nothing is darn blocked then
+(the memory usage doubles for a second there, but then the garbage collector does its job)
+
+Updated scene:
+
+    Shard:
+        []*ValueObject
+        shard_size int32
+
+    shardmanager: 
+        []*Shard
+        shardmanager_size int32
+        current_
+
+*/
 
 type Message struct {
 	data string
@@ -22,7 +48,6 @@ type ValueData struct {
 }
 
 type Shard struct {
-    // TODO: make the data a []customobject, so that you can just update that object instead of locking down the whole shard
     data []*ValueData
     size int32
 }
@@ -46,16 +71,19 @@ var keyManager = KeyManager{
 }
 
 var shardManager = ShardManager{
-    Shards: make([]*Shard, 0),
+    Shards: make([]*Shard, 1),
 }
 
 var connectionChan = make(chan net.Conn)
 var messageChan = make(chan Message)
+var curShardManagerSize = make(chan int32)
+
 var nextIdx int32 = -1
 
-func getNewShard() *Shard {
+func getNewShard(sz int32) *Shard {
+    fmt.Println("making a new shard of size: ",sz)
     return &Shard{
-        data: make([]*ValueData, 0, ShardSize),
+        data: make([]*ValueData, sz, ShardSize),
     }
 }
 
@@ -65,7 +93,51 @@ func getNewValueData(value string) *ValueData{
     }
 }
 
+func resizeShardManager(){
+    // fmt.Println(shardManager.Shards)
+    
+    for {
+        // fmt.Println("===========================================")
+        curSize := <- curShardManagerSize
+        
+        fmt.Println("im here")
+
+        if int32(len(shardManager.Shards)) >= curSize {
+            fmt.Println("Resizing the whole darn shit now!!")
+
+            // lock down the whole shit
+            shardManager.mutex.Lock()
+            
+            addSize := curSize
+            curSize *= 2
+
+            // I hope the below thing is concurrency safe....
+
+            // newShards := make([]ShardManager, 1)
+            
+            newShards := shardManager
+
+            // add addSize more shards to this shit
+            for i := len(shardManager.Shards); i < len(shardManager.Shards)+ int(addSize) ; i++ {
+                // newShards.Shard[i] = getNewShard(ShardSize)
+                newShards.Shards = append(newShards.Shards,getNewShard(1))
+            }
+
+            shardManager = newShards
+
+            shardManager.mutex.Unlock()
+
+        }
+        fmt.Println(123)
+        curShardManagerSize <- curSize
+    }
+}
+
 func _setKey(key string, value string) {
+
+    // this thing accesses the whole darn shard manager and puts a lock on it, which is not good, very very bad
+    // we are accessing the length of the number of shards and that is not what we want, we need another way to access shit man, this is bad, very very bad
+    
     // Lock keyManager to ensure thread safety for adding keys
     keyManager.mutex.Lock()
     idx, exists := keyManager.Keys[key]
@@ -83,20 +155,32 @@ func _setKey(key string, value string) {
     fmt.Println("setting key",key,"at",idx,"at shard number",shardNumber,"at local index",localShardIndex)
     
     // Lock shardManager to ensure thread safety for adding shards
+    fmt.Println("before locking")
+
+    // shardManager.mutex.Unlock() // wtf bruh :skull:
+    
     shardManager.mutex.Lock()
+    
     defer shardManager.mutex.Unlock()
 
+    fmt.Println("locked now")
+
+    fmt.Println(shardManager.Shards)
+
+
     for shardNumber >= int32(len(shardManager.Shards)) {
-        shardManager.Shards = append(shardManager.Shards, getNewShard())
+        // this is not good, we make it happen on its own!!
+        fmt.Errorf("nooooo senpaiiiiiiiiii")
     }
 
     shard := shardManager.Shards[shardNumber]
     newVal := getNewValueData(value)
 
     if int32(len(shard.data)) > localShardIndex{
-        *shard.data[localShardIndex] = *newVal
+        fmt.Println("hi mom",shard.data)
+        shard.data[localShardIndex] = newVal
     } else {
-        shard.data = append(shard.data, newVal)
+        fmt.Errorf("oh nu, hewp me daddy")
     }
 }
 
@@ -113,7 +197,7 @@ func _getKey(key string) (string, bool) {
     defer shardManager.mutex.Unlock()
 
     if shardNumber >= int32(len(shardManager.Shards)) {
-        fmt.Println("first off")
+        // fmt.Println("first off")
         return "", false
     }
 
@@ -126,7 +210,7 @@ func _getKey(key string) (string, bool) {
         return (shard.data[localShardIndex]).data, true
     }
 
-    fmt.Println("second off")
+    // fmt.Println("second off")
     // shard does not exist, should never reach here!!!!
     return "NaN", false
 }
@@ -161,6 +245,7 @@ func handleConnection(conn net.Conn) {
     fmt.Println("Client connected")
     buffer := make([]byte, 1024)
     connectionChan <- conn
+
     for {
         // Read data from the connection
         length, err := conn.Read(buffer)
@@ -205,7 +290,7 @@ func handleConnection(conn net.Conn) {
 }
 
 func main() {
-    // runtime.GOMAXPROCS(runtime.NumCPU())
+    runtime.GOMAXPROCS(runtime.NumCPU())
 
     ln, err := net.Listen("tcp", ":8080")
 
@@ -214,6 +299,11 @@ func main() {
     }
 
     go listener()
+    
+    go resizeShardManager()
+
+    shardManager.Shards[0] = getNewShard(ShardSize)
+    curShardManagerSize <- 1
 
 	fmt.Println("Server listening on :8080")
 
